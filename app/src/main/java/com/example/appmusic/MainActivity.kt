eafc20136b49a099da960b38e670b90e416e6c50
maximk0 <kisuke95@mail.ru>
2023-11-27 23:29:22 +0400

    added startMusicService()

--- app/src/main/java/com/example/appmusic/MainActivity.kt
+++ app/src/main/java/com/example/appmusic/MainActivity.kt
@@ -2,9 +2,10 @@ package com.example.appmusic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.example.appmusic.MusicPlayerService.Companion.isPaused
 import com.example.appmusic.databinding.ActivityMainBinding
 
 private const val IS_PAUSED = "playing key"
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
-    private var isPaused = true
+    private var isClickPaused = isPaused
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +23,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         savedInstanceState?. let {
-            isPaused = it.getBoolean(IS_PAUSED)
+            isClickPaused = it.getBoolean(IS_PAUSED)
         }
+
         updateButtonsState()
 
-        Log.d("MAIN_ACTIVITY", "onCreate isPaused = $isPaused")
+        Log.d("MAIN_ACTIVITY", "onCreate isPaused = $isClickPaused")
 
         binding.trackName.isSelected = true
 
@@ -34,53 +36,43 @@ class MainActivity : AppCompatActivity() {
             updateButtonsState()
 
             next.setOnClickListener {
-                isPaused = false
+                isClickPaused = false
+
+                startMusicService(Constants.ACTION.NEXT_FOREGROUND_ACTION)
                 updateButtonsState()
-                MusicPlayerService.getIntent(this@MainActivity).also {
-                    it.action = Constants.ACTION.NEXT_FOREGROUND_ACTION
-                    startService(it)
-                }
             }
 
             play.setOnClickListener {
-                isPaused = !isPaused
-                updateButtonsState()
+                isClickPaused = !isClickPaused
 
-                MusicPlayerService.getIntent(this@MainActivity).also {
-                    it.action = Constants.ACTION.START_FOREGROUND_ACTION
-                    startService(it)
-                }
+                startMusicService(Constants.ACTION.START_FOREGROUND_ACTION)
+                updateButtonsState()
             }
 
             pause.setOnClickListener {
-                isPaused = !isPaused
-                updateButtonsState()
+                isClickPaused = true
 
-                MusicPlayerService.getIntent(this@MainActivity).also {
-                    it.action = Constants.ACTION.STOP_FOREGROUND_ACTION
-                    startService(it)
-                }
+                startMusicService(Constants.ACTION.STOP_FOREGROUND_ACTION)
+                updateButtonsState()
             }
 
             previous.setOnClickListener {
-                isPaused = false
+                isClickPaused = false
+
+                startMusicService(Constants.ACTION.PREVIOUS_FOREGROUND_ACTION)
                 updateButtonsState()
-                MusicPlayerService.getIntent(this@MainActivity).also {
-                    it.action = Constants.ACTION.PREVIOUS_FOREGROUND_ACTION
-                    startService(it)
-                }
             }
         }
 
     }
 
     private fun updateButtonsState() {
-        if (!isPaused) {
+        if (!isClickPaused) {
             binding.pause.visibility = View.VISIBLE
             binding.play.visibility = View.GONE
             binding.trackName.text = getString(
                 R.string.track_name,
-                resources.getResourceEntryName(R.raw.imagine_dragons_im_so_sorry)
+                resources.getResourceEntryName(MusicPlayerService.soundtrackName)
             )
             binding.lottieAnimationView.playAnimation()
         } else {
@@ -90,16 +82,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun startMusicService(action: String) {
+        MusicPlayerService.getIntent(this).also {
+            it.action = action
+            ContextCompat.startForegroundService(this, it)
+        }
+    }
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putBoolean(IS_PAUSED, isPaused)
-        Log.d("MAIN_ACTIVITY", "onSaveInstanceState isPaused = $isPaused")
+        outState.putBoolean(IS_PAUSED, isClickPaused)
+        Log.d("MAIN_ACTIVITY", "onSaveInstanceState isPaused = $isClickPaused")
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        Log.d("MAIN_ACTIVITY", "onRestoreInstanceState isPaused = $isPaused")
-        isPaused = savedInstanceState.getBoolean(IS_PAUSED)
+        Log.d("MAIN_ACTIVITY", "onRestoreInstanceState isPaused = $isClickPaused")
+        isClickPaused = savedInstanceState.getBoolean(IS_PAUSED)
         updateButtonsState()
     }
 
